3dbaf7be3658b9a4f6683467bb9fe6cf5b002cf5
Rakesh Swain <rakesh.swain@netprophetsglobal.com>
2025-03-21 12:21:27 +0530

    Initial commit for adding new project

---
+++ src/main/java/com/tech/user_insights/UserInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.user_insights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/user_insights/UserInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.user_insights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
